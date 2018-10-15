9874215dde0907d988024df1ad390104992fff9c
Camron Fritz <camfritz@gmail.com>
2018-10-15 11:51:46 -0700

    Initial Commit.

---
+++ app/src/main/java/edu/fritzcamoregonstate/fritzcamronexampractice1/MainActivity.java
@@ -0,0 +1,45 @@
+package edu.fritzcamoregonstate.fritzcamronexampractice1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mMangleButton;
+    private EditText mFirstNameText;
+
+    private String[] lastNames = new String[] {
+            "Simmons",
+            "Matthews",
+            "Rubin",
+            "Nichols",
+            "Blair"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMangleButton = (Button) findViewById(R.id.mangle_button);
+        mFirstNameText = (EditText) findViewById(R.id.first_name_field);
+
+        mMangleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+                int randomNumber = random.nextInt(4 + 1 - 0);
+
+                Toast.makeText(MainActivity.this, mFirstNameText.getText() + " " + lastNames[randomNumber], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
